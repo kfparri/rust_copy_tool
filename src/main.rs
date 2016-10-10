9a1fdcdb16661a5c55e8cccac5ca421d83e2a079
Kyle Parrish <kfparri@gmail.com>
2016-10-09 22:41:03 -0500

    I've decided to change the way the program works.  I don't want this app to traverse folders.

--- src/main.rs
+++ src/main.rs
@@ -22,70 +22,17 @@ fn main() {
     //  if there are not exactly one argument (one plus the program), exit the program.
     if args.len() != 2  {
         // print program usage
-        println!("Usage: file_copy_tool <Root Folder Path>");
+        println!("Usage: file_copy_tool <Copy Folder Path>");
         
         // exit the program
-        //std::process::exit(0);
+        std::process::exit(0);
     }
 
-    let paths = fs::read_dir("./").unwrap();
+    println!("{}", args[1]);
 
-    for path in paths {
-        //println!("Name: {}", path.unwrap().path().display())
-        let filename = path.unwrap().path().as_os_str();
-
-        println!("{}", filename.to_str());
-        //let attr = fs::metadata(path.unwrap().path().display()).unwrap();
-
-        //if attr.is_dir() {
-            //println!("{}, is a directory!", path.unwrap().path().display());
-        //}
-        println!("Name: {}", path.unwrap().path().display());
-    }
-
-    let startFolder = ".";
-    copyFile(startFolder);
-
-    /*
-    let temppath = Path::new(&args[1]); //"testfile.json");
-    let path = temppath.join("testfile.json");
-
-    let display = path.display();
-
-    //let path = Path::new("testfile.json");
-    //let display = path.display();
-
-    // matching is like error checking in rust.  This match tries to open a file and switches through based on 
-    //  what the result is (Err, Ok, etc.)
-    let mut file =  match File::open(&path) {
-        Err(why) => panic!("Couldn't open {}: {}", display, why.description()),
-        Ok(file) => file
-    };//try!(File::open("testfile.json"));
-
-    // define a string for the file contents
-    let mut file_contents = String::new();
-
-    match file.read_to_string(&mut file_contents) { 
-        Err(why) => panic!("Coulding read {}: {}", display, why.description()),
-        Ok(_) => print!("{} contains: \n{}", display, file_contents),
-    };
-
-    //try!(file.read_to_string(&mut fileContents));
-
-    //println!("contents: {}", fileContents);
-
-    let decoded: TransferParams = json::decode(&file_contents).unwrap();
-
-    //println!("file_name: {}", decoded.file_name);
-    //println!("Copy path: {}", decoded.copy_to_path);
-
-    //println!("Trying to copy {} to {}", decoded.file_name, decoded.copy_to_path);
-
-    match fs::copy(decoded.file_name, decoded.copy_to_path) {
-        Err(why) => panic!("Couldn't copy the file {}: {}", display, why.description()),
-        Ok(_) => println!("File copy complete!")
-    };
-    */
+    let test = args[1].clone();
+    
+    copyFile(&test);
 }
 
 fn copyFile( folder_path: &str ) {
